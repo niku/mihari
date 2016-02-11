a1d8ba51d799bb3d8f135d12f91b52ddef26b7b6
niku <niku@niku.name>
2016-02-11 19:10:04 +0900

    Add Mihari.Supervisor

---
+++ lib/mihari/supervisor.ex
@@ -0,0 +1,15 @@
+defmodule Mihari.Supervisor do
+  use Supervisor
+
+  def start_link do
+    Supervisor.start_link(__MODULE__, [], name: __MODULE__)
+  end
+
+  def init(_args) do
+    children = [
+      worker(Mihari.Worker, [], restart: :temporary)
+    ]
+    opts = [strategy: :simple_one_for_one]
+    supervise(children, opts)
+  end
+end
